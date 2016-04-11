a5462762fecdff048802aa162b31c12071c222c7
antonov_727 <antonov_727@mail.ru>
2016-04-11 07:09:07 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -189,7 +189,7 @@ jQuery(document).ready(function($) {
         var $input = $(this).parent().find('input');
         var count = parseInt($input.val()) - 1;
         count = count < 1 ? 1 : count;
-        $input.val(count);
+        $input.val(count+' шт');
         $input.change();
         return false;
     });
